2301aae7cb391ad1ee748dae504590d1202d6557
wangxin20 <wangxin20@kuaishou.com>
2022-07-27 14:03:30 +0800

    feat: update dev

--- dev.config.js
+++ dev.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  request: {
-    // '/rest': {
-    //   target: 'http://127.0.0.1:7001',
-    //   headers: {},
-    // },
-    // '/rest/jia/search': {
-    //   target: 'https://chitu.staging.kuaishou.com',
-    //   headers: {},
-    // },
-  },
+  request: {},
 };
